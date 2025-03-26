11c3ca8fe44cd93fec8e0e0d59c2d77a1d31fff0
Cédric <cedricvanhoorebeke@outlook.com>
2025-03-26 11:24:25 +0100

    Multer

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from "express";
 import chalk from "chalk";
 import { PORT } from "./utils/envs";
 import multer from "multer";
+import path from "path";
 
 // Variables
 const app = express();
@@ -14,9 +15,33 @@ app.use(express.json());
 app.use(express.static("src/public"));
 app.use("/uploads", express.static("uploads"));
 
-const upload = multer({ dest: "uploads/", limits: { fileSize: 1000000 } });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const newName =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(null, newName);
+  },
+});
+
+const upload = multer({ storage, limits: { fileSize: 3000000 } });
 
 // Routes
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    res.status(200).send("File uploaded successfully!");
+    return;
+  }
+  const imageUrl = `/uploads/${req.file.filename}`;
+  console.log(imageUrl);
+  res.status(200).send(`
+    <h1>Image uploaded successfully!</h1>
+    <img src="http://localhost:3000${imageUrl}" alt="Uploaded Image" with="200" height="200"/>
+  `);
+});
+
 app.get("/", (req, res) => {
   res.sendFile("/index.html");
 });
